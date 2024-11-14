03bf2daad2acf6b5dacb255cf01b70393d240b48
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-11-14 09:41:08 -0500

    update readme;

--- obj/Debug/net8.0-windows7.0/Bubba.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows7.0/Bubba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bubba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b122b2d6c7b5c19fcaab58c608f6148ac5985a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e6911e02fa914c260c3c41f56564e44c9b39eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bubba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bubba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
