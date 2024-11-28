9b7be4b8587c1db0e19d73f5b19de436e90202b1
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-11-27 19:09:10 -0500

    update

--- obj/Debug/net8.0-windows/Bubba.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Bubba.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A small and simple windows (wpf) application for interacting with the OpenAI Comp" +
     "letions and Assistants API that\'s developed in C-Sharp under the MIT license. ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9dbd8349cbeac7f164fc03bbef691e1d2ba8bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d3dfdeb6c26a938dc8163214c56f56dfe062c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bubba GPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bubba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
